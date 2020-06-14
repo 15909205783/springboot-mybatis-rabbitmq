1310cfbce1f1c6aaad06e4d49b721a645b4e09bc
yangfan <2585928225@qq.com>
2020-06-14 23:38:46 +0800

    Initial commit

---
+++ src/main/java/com/yangfan/springbootmybatisrabbitmq/SpringbootMybatisRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.yangfan.springbootmybatisrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yangfan/springbootmybatisrabbitmq/SpringbootMybatisRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangfan.springbootmybatisrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
